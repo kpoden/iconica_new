865ee3c3cd8549eaf60c68e4bffb122f99d7b83b
Konstantin Podenezhniy <67436313+kpoden@users.noreply.github.com>
2023-08-23 12:58:36 +0500

    forms fixes

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -36,10 +36,10 @@ function teamSlider() {
         slidesPerView: 2,
         spaceBetween: 20,
         loop: true,
-        // autoplay: {
-        //     delay: 3000,
-        //     disableOnInteraction: false
-        //   },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false
+          },
           speed: 1000,
           breakpoints: {  
             '450': {
@@ -76,7 +76,7 @@ function teamSlider() {
 
 
 
-
+teamSlider()
 
 
 function certSlider() {
@@ -85,22 +85,22 @@ function certSlider() {
       slidesPerView: 1.5,
       spaceBetween: 10,
       loop: true,
-      // autoplay: {
-      //     delay: 4000,
-      //     disableOnInteraction: false
-      //   },
+      autoplay: {
+          delay: 4000,
+          disableOnInteraction: false
+        },
         speed: 1000,
         breakpoints: {  
-          '500': {
-            slidesPerView: 2,
+          '1000': {
+            slidesPerView: 1.5,
             spaceBetween: 20, },
-          '700': {
+          '1250': {
             slidesPerView: 2.5,
             spaceBetween: 20, },
-          '1000': {
+          '1500': {
             slidesPerView: 3,
             spaceBetween: 32, },
-          '1500': {
+          '1700': {
             slidesPerView: 3.5,
             spaceBetween: 32, },
         },
@@ -110,18 +110,18 @@ function certSlider() {
     const arrNext = document.querySelector('.cert__arrs .arr-r');
 
     arrPrev.addEventListener('click', () => {
-      teamSlider.slidePrev();
+      certSlider.slidePrev();
     })
     
     arrNext.addEventListener('click', () => {
-      teamSlider.slideNext();
+      certSlider.slideNext();
     })
   }
     
 }
 
 
-
+certSlider()
 
 
 
@@ -243,3 +243,339 @@ function dropDown() {
 }
 
 dropDown()
+
+
+
+function bodyLock(con) {
+  const scrollFix = window.innerWidth - document.body.clientWidth + "px";
+  if (con === true) {
+    document.body.classList.add("_lock");
+    document.body.style.paddingRight = scrollFix;
+  } else if (con === false) {
+    document.body.classList.remove("_lock");
+    document.body.style.paddingRight = 0;
+  } else if (con === undefined) {
+    if (!body.classList.contains("_lock")) {
+      document.body.classList.add("_lock");
+      document.body.style.paddingRight = scrollFix;
+    } else {
+      document.body.classList.remove("_lock");
+      document.body.style.paddingRight = 0;
+    }
+  } else {
+    console.error("Неопределенный аргумент у функции bodyLock()");
+  }
+}
+
+
+
+
+class Modal {
+  constructor(template = "") {
+      this.modal = document.querySelector('.modal');
+      this.template = template;
+      this.container = document.querySelector('.modal__content-inner');
+      this.init();
+  }
+
+
+  listenOpenTriggers() {
+      this.triggers = document.querySelectorAll('[data-modal]');
+      this.triggers.forEach((trigger)=>{
+          trigger.addEventListener('click', () => {
+              this.templateName = trigger.dataset.modal;
+              
+              this.openModal();
+          });
+      });
+  }
+
+  listenOpenTriggersModal() {
+      this.triggers = this.modal.querySelectorAll('[data-modal]');
+      this.triggers.forEach((trigger)=>{
+        
+          trigger.addEventListener('click', (e) => {
+              e.preventDefault();
+              this.templateName = trigger.dataset.modal;
+              
+              this.openModal();
+          });
+      });
+  }
+
+  listenCloseTriggers() {
+      this.close = document.querySelectorAll('.modal__close');
+      this.close.forEach((close) => {
+          close.addEventListener('click', () => {
+              this.closeModal();
+          });
+        });
+
+        this.modal.addEventListener("click", (e) => {
+          e = e.target;
+          if(e.classList.contains('modal')) {
+              this.closeModal()
+          }
+        });
+  }
+
+  initTemplate() {
+      if(document.querySelector('.'+this.templateName)) {
+          this.template = document.querySelector('.'+this.templateName);
+          this.container.innerHTML = this.template.innerHTML;
+          if(this.container.querySelector('input[type=file]')) {
+            formAttach();
+          }
+      }
+
+      
+  }
+
+  initForms() {
+      if(this.modal.querySelector('form')) {
+          const form = this.modal.querySelector('form').id;
+          const formInit = new Form(form);
+      }
+  }
+
+  openModal() {
+      this.modal.classList.add('opened');
+      bodyLock(true);
+      this.initTemplate();
+      this.initForms();
+      this.listenCloseTriggers();
+      this.listenOpenTriggersModal();
+  }
+
+  closeModal() {
+      this.modal.classList.remove('opened');
+      bodyLock(false);
+  }
+
+
+
+
+  init() {
+      this.listenOpenTriggers();
+      this.listenCloseTriggers()
+      
+  }
+}
+
+
+
+
+const modal = new Modal();
+
+
+function formAttach() {
+
+  document.querySelectorAll('.modal__attach input[type=file]').forEach((el)=>{
+    el.addEventListener('change', function() {
+      console.log(2);
+    const file = this.files[0];
+    const parent = this.parentNode;
+    const textElement = parent.querySelector('.modal__attach-text');
+    console.log(file.name);
+    textElement.innerHTML = file.name;
+    if (file && file.size > 10 * 1024 * 1024) {
+      textElement.innerHTML = 'Максимальный размер файла 10Мб';
+      textElement.classList.add('_error');
+      console.log('max file size');
+    } else {
+      textElement.innerHTML = file ? file.name : '';
+      textElement.classList.remove('_error');
+      
+    }
+  });
+})
+}
+
+formAttach();
+
+
+
+
+
+
+
+
+
+class Form {
+  constructor(form) {
+
+      this.form = document.getElementById(form);
+      this.init()
+
+  }
+
+  deleteErrors() {
+      const errors = this.form.querySelectorAll('._error-txt');
+      errors.forEach((el) => {
+        const parent = el.parentNode;
+        parent.removeChild(el);
+      });
+  
+  
+      const inputs = this.form.querySelectorAll('input[type=text], input[type=tel], input[type=password]');
+      inputs.forEach((input) => {
+        input.classList.remove('_error-input');
+      });
+
+      if(this.form.querySelector('.error-wrap')) {
+          this.form.querySelectorAll('.error-wrap').forEach(el=>el.textContent="");
+      }
+ 
+  }
+
+  createError(err) {
+
+      if(this.form.id == "callback-form" || 1) {
+     
+          let errorName = "Обязательное поле";
+      
+          err.forEach((el) => {
+              const inputWrap = el.closest('.input-wrap');
+              let errorEl = document.createElement('span');
+              errorEl.textContent = errorName;
+              errorEl.classList.add('_error-txt');
+              el.classList.add('_error-input');
+              inputWrap.appendChild(errorEl);
+      
+          });
+    } 
+  }
+
+
+  validateForm() {
+      const requiredInputs = this.form.querySelectorAll('[data-required]');
+      
+      let errorArr = [];
+      this.deleteErrors();
+
+      requiredInputs.forEach(el => {
+          const value = el.value.trim();
+          if(value === "") {
+              errorArr.push(el);
+          }
+      })
+
+      if(errorArr.length > 0) {
+      this.createError(errorArr);
+      
+      return false;
+      } else {
+      return true;
+      }
+
+  }
+
+
+  policyChecked() {
+
+      const check = this.form.querySelector('.policy__checkbox-input');
+      const chechErr = this.form.querySelector('.policy-err');
+      if(!check.checked) {
+          let errorName = "Ознакомьтесь с политикой конфиденциальности";
+          let errorEl = document.createElement('span');
+          errorEl.textContent = errorName;
+          errorEl.classList.add('_error-txt');
+          chechErr.appendChild(errorEl);
+
+          return false
+      } else {
+          return true
+      }
+  }
+
+  validateEmail() {
+      const emailInput = this.form.querySelector('input[name=email]');
+      const email = emailInput.value;
+      const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      let validEmail = emailPattern.test(email);
+      if(!validEmail) {
+          console.log('email not val');
+          const errorName = "Введите верный Email";
+          const inputWrap = emailInput.closest('.input-wrap');
+          let errorEl = document.createElement('span');
+          errorEl.textContent = errorName;
+          errorEl.classList.add('_error-txt');
+          emailInput.classList.add('_error-input');
+          inputWrap.appendChild(errorEl);
+          return false;
+      } else {
+          return true;
+      }
+  }
+
+  createSuccsessMsg() {
+
+      if(this.form.id == 'vacancy-form') {
+          document.querySelector('.thanksVakancyLink').click();
+      } if(this.form.id == 'callback-form') {
+          document.querySelector('.thanksCallbackLink').click();
+      }
+  }
+
+
+  listenSubmit() {
+
+      this.form.addEventListener('submit', (e) => {
+          e.preventDefault();
+
+          if(!this.validateForm()) {
+              return;
+            }
+
+          if(!this.policyChecked()) {
+              return;
+            }
+
+
+
+          const xhr = new XMLHttpRequest();
+          let formData = new FormData(this.form);
+          console.log(formData);
+          console.log('send form data');
+  
+          if(this.form.id == 'vacancy-form') {
+              xhr.open('POST', 'local/ajax/vacancy.php');
+          
+          } else if(this.form.id =='callback-form') {
+
+              xhr.open('POST', '/local/ajax/callback.php');
+
+          } 
+            
+          xhr.onload = () => {
+              if (xhr.status === 200) {
+                  console.log(xhr.responseText);
+              } else {
+                  this.createSuccsessMsg();
+                  console.log(xhr.responseText);
+              }
+          };
+
+          xhr.send(formData);
+          this.form.classList.add('sent');
+          this.form.reset();
+
+      });
+  }
+
+  maskInit() {
+      if(this.form.querySelector('input[name=phone]')) {
+      
+      const phoneInput = this.form.querySelector('input[name=phone]');
+      const maskOptions = {
+        mask: '+{7}(000) 000-00-00'
+      };
+      const mask = IMask(phoneInput, maskOptions);
+      }
+  }
+
+  init() {
+      this.listenSubmit();
+      this.maskInit();
+  }
+}
